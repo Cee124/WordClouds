177e0c1c99315197106154934033855ccf19530a
Christian Eisele <3015571@stud.hs-mannheim.de>
2025-04-30 12:45:13 +0200

    Initial repository setup: Added base packages and classes for project structure

---
+++ Wordclouds/src/main/java/de/eisele/wordcloud/domain/WordCounter.java
@@ -0,0 +1,5 @@
+package de.eisele.wordcloud.domain;
+
+public class WordCounter {
+
+}
---
+++ Wordclouds/src/main/java/de/eisele/wordcloud/persistence/PDFFileReader.java
@@ -0,0 +1,24 @@
+package de.eisele.wordcloud.persistence;
+
+import java.io.File;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class PDFFileReader {
+
+	public String readPDFFile(File pdfFile) {
+		String text="";
+		try (PDDocument document = PDDocument.load(pdfFile)) {
+			PDFTextStripper pdfStripper = new PDFTextStripper();
+			text = pdfStripper.getText(document);
+			System.out.println(text);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return text;
+
+	}
+	
+}
